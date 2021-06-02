a512a7447b17c596054063f9bd071d7ffcf97be7
RJR182424 <robertjes.389@gmail.com>
2021-06-02 14:44:47 -0500

    MedicoDataAnnotations

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -6,9 +6,12 @@ namespace Turnos.Models
     public class Especialidad
     {
 
-        [Key]
+        [Key] /*esto es un Data Annotations*/
         public int IdEspecialidad { get; set; }
         /*Primary key de la tabla*/
+        [StringLength(200, ErrorMessage ="El campo descripción debe tener como máximo 200 caracteres")]
+        [Required (ErrorMessage = "Debe ingresar una descripción")]
+        [Display (Name = "Descripción", Prompt = "Ingrese una descripción")]
         public string Descripcion { get; set; }
         public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
 
--- Models/Medico.cs
+++ Models/Medico.cs
@@ -8,12 +8,26 @@ namespace Turnos.Models
     {
         [Key]
         public int IdMedico { get; set; }
+        [Required(ErrorMessage ="Debe ingresar un nombre")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage ="Debe ingresar un apellido")]
         public string Apellidos { get; set; }
+        [Required(ErrorMessage ="Debe ingresar un dirección")]
+        [Display (Name = "Dirección")]
         public string Direccion { get; set; }
+        [Required(ErrorMessage ="Debe ingresar un teléfono")]
+        [Display (Name = "Teléfono")]
         public string Telefono { get; set; }
+        [Required(ErrorMessage ="Debe ingresar un email")]
+        [EmailAddress (ErrorMessage ="No es una direccion de email valida")]
         public string Email { get; set; }
+        [Display (Name = "Horario Desde")]
+        [DataType (DataType.Time)]
+        [DisplayFormat (DataFormatString ="{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime HorarioAtencionDesde { get; set; }
+        [Display (Name = "Horario Hasta")]
+        [DataType (DataType.Time)]
+        [DisplayFormat (DataFormatString ="{0:hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime HorarioAtencionHasta { get; set; }
         public  List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
     }
