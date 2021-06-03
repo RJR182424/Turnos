2b63b93d87f227bea15d59fe3f977fee752c2fe8
RJR182424 <robertjes.389@gmail.com>
2021-06-02 23:14:44 -0500

    DataAnnotations

--- Models/Paciente.cs
+++ Models/Paciente.cs
@@ -4,12 +4,18 @@ namespace Turnos.Models{
     public class Paciente{
         [Key]
         public int IdPaciente{get; set;}
-
+        [Required(ErrorMessage ="Debe ingresar un nombre")]
         public string Nombre{get; set;}
-
+        [Required(ErrorMessage ="Debe ingresar un apellido")]
         public string Apellidos{get; set;}
+        [Required(ErrorMessage ="Debe ingresar un dirección")]
+        [Display (Name = "Dirección")]
         public string Direccion{get; set;}
+        [Required(ErrorMessage ="Debe ingresar un teléfono")]
+        [Display (Name = "Teléfono")]
         public string Telefono{get; set;}
+        [Required(ErrorMessage ="Debe ingresar un email")]
+        [EmailAddress (ErrorMessage ="No es una direccion de email valida")]
         public string Email{get; set;}
     }
 }
